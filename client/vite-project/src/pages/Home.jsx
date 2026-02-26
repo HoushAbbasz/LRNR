7d73effb8100242ccd57149daa21d27b02b160c6
Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
2026-02-26 11:02:04 -0500

    Build: Building homepage, adding fonts, and variables for styling

--- client/vite-project/src/pages/Home.jsx
+++ client/vite-project/src/pages/Home.jsx
@@ -1,22 +1,34 @@
 // Home page component - will be built out later
+const topics = ['golang', 'aws', 'javascript', 'CI/CD', 'home gardens', 'coffee', 'finger foods']
+
+
 function Home() {
   return (
   <div id="Home-page">
-    <h2>THIIS IS THE HOME PAGE</h2>
     <section id="hero">
       <div id="hero-text">
-        
+        <h2>LRNR</h2>
+        <p>Your guided path to programming englightenment</p>
       </div>
-      <div id="topic-bubbles">
-        
-      </div>
-      <div id="begin-banner">
 
-      </div>
-      <div id="description-cards">
-        
+      <div id="topic-bubbles">
+        <h2>TOPIC BUBBLES</h2>
       </div>
     </section>
+    {/* MOBILE USE */}
+    <section id="topic-carousel">
+      <p>topics n stuff</p>
+    </section>
+
+    <section id="begin-banner">
+      <h2>TAKE A QUIZ!</h2>
+      <button>Get Started<img src="/images/right-arrow.png"></img></button>
+    </section>
+    <div id="description-cards">
+      <h2>card 1</h2>
+      <h2>card 2</h2>
+      <h2>card 3</h2>
+    </div>
   </div>
   )
 }
